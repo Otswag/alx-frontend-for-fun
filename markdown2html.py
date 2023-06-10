bc708edbfde827edc687baccbc4655fb3762c2e6
Otswag <106761383+Otswag@users.noreply.github.com>
2023-06-11 00:21:06 +0100

    Debugging markdown html

--- markdown2html.py
+++ markdown2html.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
+
 """
+
 Converts a Markdown file to an HTML file.
 
 Usage: ./markdown2html.py <input_file> <output_file>
+
 """
 
 import sys
+
 import markdown
 
-if len(sys.argv) < 3:
+if len(sys.argv) != 3:
+
     print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+
     sys.exit(1)
 
 input_file = sys.argv[1]
+
 output_file = sys.argv[2]
 
 try:
+
     with open(input_file, 'r') as f:
+
         markdown_text = f.read()
+
 except FileNotFoundError:
+
     print(f"Missing {input_file}", file=sys.stderr)
+
     sys.exit(1)
 
 html_text = markdown.markdown(markdown_text)
 
 with open(output_file, 'w') as f:
+
     f.write(html_text)
 
-sys.exit(0)
+if __name__ == "__main__":
+
+    sys.exit(0)
+ 
