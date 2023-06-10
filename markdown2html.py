564b09f06722a8c194c8eb24e873b4019bd01951
Otswag <106761383+Otswag@users.noreply.github.com>
2023-06-11 00:59:24 +0100

    Debugging markdown html 1.12

--- markdown2html.py
+++ markdown2html.py
@@ -1,58 +1,83 @@
 #!/usr/bin/python3
 
+"""
+
+A script that converts Markdown to HTML.
+
+"""
+
 import sys
 
 import os
 
-import markdown
-
 import re
 
-def markdown2html(markdown_file, output_file):
+def convert_markdown_to_html(input_file, output_file):
 
-    # Check if the markdown file exists
+    """
 
-    if not os.path.isfile(markdown_file):
+    Converts a Markdown file to HTML and writes the output to a file.
 
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    """
+
+    # Check that the Markdown file exists and is a file
+
+    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
+
+        print(f"Missing {input_file}", file=sys.stderr)
 
         sys.exit(1)
 
-    # Read the markdown file content
+    # Read the Markdown file and convert it to HTML
 
-    with open(markdown_file, 'r') as md:
+    with open(input_file, encoding="utf-8") as f:
 
-        markdown_content = md.read()
+        html_lines = []
 
-    # Convert markdown to HTML
+        for line in f:
 
-    html_content = markdown.markdown(markdown_content)
+            # Check for Markdown headings
 
-    # Perform additional processing using regular expressions
+            match = re.match(r"^(#+) (.*)$", line)
 
-    html_content = re.sub(r'<h1>', r'<h1 style="color: red;">', html_content)
+            if match:
 
-    # Write the HTML content to the output file
+                heading_level = len(match.group(1))
 
-    with open(output_file, 'w') as html:
+                heading_text = match.group(2)
 
-        html.write(html_content)
+                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+
+            else:
+
+                html_lines.append(line.rstrip())
+
+    # Write the HTML output to a file
+
+    with open(output_file, "w", encoding="utf-8") as f:
+
+        f.write("\n".join(html_lines))
 
 if __name__ == "__main__":
 
-    # Check the number of arguments
+    # Check that the correct number of arguments were provided
 
     if len(sys.argv) != 3:
 
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>", file=sys.stderr)
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
 
         sys.exit(1)
 
-    markdown_file = sys.argv[1]
+    # Get the input and output file names from the command-line arguments
+
+    input_file = sys.argv[1]
 
     output_file = sys.argv[2]
 
-    markdown2html(markdown_file, output_file)
+    # Convert the Markdown file to HTML and write the output to a file
+
+    convert_markdown_to_html(input_file, output_file)
+
+    # Exit with a successful status code
 
     sys.exit(0)
-
